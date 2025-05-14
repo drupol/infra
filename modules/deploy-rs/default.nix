ccc79b1915e93beaed348c24096b2083e899efe8
Pol Dellaiera <pol.dellaiera@protonmail.com>
2025-05-14 15:46:46 +0200

    refactor: use `unify` framework

--- modules/deploy-rs/default.nix
+++ modules/deploy-rs/default.nix
@@ -5,43 +5,32 @@
   ...
 }:
 {
+  imports = [
+    inputs.make-shell.flakeModules.default
+  ];
+
+  flake = {
+    deploy.nodes = lib.mapAttrs (
+      hostname: hostConfig:
+      let
+        inherit (inputs.self.nixosConfigurations.${hostname}.config.nixpkgs.hostPlatform) system;
+      in
+      {
+        hostname = hostConfig.fqdn;
+        fastConnection = false;
+        profiles.system = {
+          user = "root";
+          sshUser = "pol";
+          sshOpts = [ ];
+          path = inputs.deploy-rs.lib.${system}.activate.nixos inputs.self.nixosConfigurations.${hostname};
+        };
+      }
+    ) config.unify.hosts;
+  };
+
   perSystem =
     { pkgs, ... }:
     {
-      make-shells.default = {
-        packages = [
-          pkgs.deploy-rs
-        ];
-      };
-    };
-
-  flake =
-    let
-      prefix = "hosts/";
-    in
-    {
-      deploy.nodes = lib.pipe config.flake.modules.nixos [
-        (lib.filterAttrs (name: _: lib.hasPrefix prefix name))
-        (lib.mapAttrs' (
-          name: module:
-          let
-            hostname = lib.removePrefix prefix name;
-            inherit (inputs.self.nixosConfigurations.${hostname}.config.nixpkgs.hostPlatform) system;
-          in
-          {
-            name = hostname;
-            value = {
-              inherit hostname;
-              fastConnection = false;
-              profiles.system = {
-                user = "root";
-                sshUser = "pol";
-                sshOpts = [ ];
-                path = inputs.deploy-rs.lib.${system}.activate.nixos inputs.self.nixosConfigurations.${hostname};
-              };
-            };
-          }
-        ))
-      ];
+      make-shells.default.packages = with pkgs; [ deploy-rs ];
     };
 }
--- modules/desktop/environment/default.nix
+++ modules/desktop/environment/default.nix
@@ -1,92 +1,69 @@
 {
-  inputs,
-  ...
-}:
-{
-  flake.modules = {
-    nixos.desktop =
-      { pkgs, ... }:
-      {
-        xdg = {
-          portal = {
-            enable = true;
-            config.common.default = "kde";
-            extraPortals = with pkgs; [ kdePackages.xdg-desktop-portal-kde ];
-          };
-        };
-
-        services = {
-          xserver = {
-            enable = true;
-            xkb = {
-              options = "eurosign:e";
-            };
-          };
-          desktopManager = {
-            plasma6 = {
-              enable = true;
-            };
-          };
-          displayManager = {
-            sddm = {
-              enable = true;
-            };
-          };
-        };
-
-        # Only for Winbox
-        networking = {
-          firewall.allowedUDPPorts = [ 5678 ];
+  unify.modules.desktop.nixos =
+    { pkgs, ... }:
+    {
+      xdg = {
+        portal = {
+          enable = true;
+          config.common.default = "kde";
+          extraPortals = with pkgs; [ kdePackages.xdg-desktop-portal-kde ];
         };
       };
 
-    homeManager.desktop =
-      { pkgs, lib, ... }:
-      {
-        nixpkgs = {
-          config.allowUnfree = true;
-          overlays = [
-            inputs.self.overlays.default
-          ];
-        };
-
-        home = {
-          activation = {
-            # Remove ksycoca cache on activation
-            # So that KDE applications can pick up new .desktop files
-            # And it doesn't break my favorite applications shortcuts
-            nuke-ksycoca = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-              rm -fv $XDG_CACHE_HOME/ksycoca*
-            '';
+      services = {
+        xserver = {
+          enable = true;
+          xkb = {
+            options = "eurosign:e";
           };
-
-          packages = with pkgs; [
-            kdePackages.akonadi-search
-            kdePackages.akregator
-            kdePackages.ark
-            kdePackages.filelight
-            kdePackages.kate
-            kdePackages.kcalc
-            kdePackages.kdialog
-            kdePackages.kgpg
-            kdePackages.kpipewire
-            kdePackages.krdc
-            kdePackages.krfb
-            kdePackages.ksystemlog
-            kdePackages.kweather
-            kdePackages.okular
-            kdePackages.plasma-browser-integration
-            kdePackages.sddm-kcm
-            kdePackages.spectacle
-            kdePackages.xdg-desktop-portal-kde
-            kdePackages.yakuake
-            krita
-            vlc
-            winbox4
-            zotero
-            pkgs.local.gh-flake-update
-          ];
+        };
+        desktopManager = {
+          plasma6 = {
+            enable = true;
+          };
+        };
+        displayManager = {
+          sddm = {
+            enable = true;
+          };
         };
       };
-  };
+
+      # Only for Winbox
+      networking = {
+        firewall.allowedUDPPorts = [ 5678 ];
+      };
+    };
+
+  unify.modules.desktop.home =
+    { pkgs, ... }:
+    {
+      nixpkgs.config.allowUnfree = true;
+
+      home.packages = with pkgs; [
+        kdePackages.akonadi-search
+        kdePackages.akregator
+        kdePackages.ark
+        kdePackages.filelight
+        kdePackages.kate
+        kdePackages.kcalc
+        kdePackages.kdialog
+        kdePackages.kgpg
+        kdePackages.kpipewire
+        kdePackages.krdc
+        kdePackages.krfb
+        kdePackages.ksystemlog
+        kdePackages.kweather
+        kdePackages.okular
+        kdePackages.plasma-browser-integration
+        kdePackages.sddm-kcm
+        kdePackages.spectacle
+        kdePackages.xdg-desktop-portal-kde
+        kdePackages.yakuake
+        krita
+        vlc
+        winbox4
+        zotero
+      ];
+    };
 }
