bb0f0ca0a13d6d78ebc8514be467a566cce35cd7
Pol Dellaiera <pol.dellaiera@protonmail.com>
2025-06-21 07:31:36 +0200

    documentation: disable some stuff I don't use

--- modules/base/documentation.nix
+++ modules/base/documentation.nix
@@ -1,6 +1,10 @@
 {
   flake.modules.nixos.base = {
     # https://mastodon.online/@nomeata/109915786344697931
-    documentation.nixos.enable = false;
+    documentation = {
+      enable = false;
+      doc.enable = false;
+      info.enable = false;
+    };
   };
 }
