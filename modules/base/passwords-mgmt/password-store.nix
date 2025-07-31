5c8717f0392e1d112d5362d53af81dfed987299a
Pol Dellaiera <pol.dellaiera@protonmail.com>
2025-07-31 21:12:45 +0200

    Revert "password-mgmt: switch from `gopass` to `ripasso-cursive`"
    This reverts commit 938d8498824314acb2479a1924ed12f788dd3d05.

--- modules/base/passwords-mgmt/password-store.nix
+++ modules/base/passwords-mgmt/password-store.nix
@@ -6,7 +6,7 @@ topLevel: {
         programs = {
           password-store = {
             enable = true;
-            package = pkgs.ripasso-cursive;
+            package = pkgs.gopass;
             settings = {
               PASSWORD_STORE_DIR = "${config.xdg.configHome}/.password-store";
               PASSWORD_STORE_KEY = topLevel.config.flake.meta.users.${config.home.username}.key;
