3c5e9d1b2ae858bc36f308a0a83705e38a077e27
Pol Dellaiera <pol.dellaiera@protonmail.com>
2025-08-05 23:00:50 +0200

    gpg: move to base profile

---
+++ modules/base/gpg.nix
@@ -0,0 +1,16 @@
+topLevel: {
+  flake.modules = {
+    homeManager.base =
+      { config, ... }:
+      {
+        programs = {
+          gpg = {
+            enable = true;
+            settings = {
+              default-key = topLevel.config.flake.meta.users.${config.home.username}.key;
+            };
+          };
+        };
+      };
+  };
+}
