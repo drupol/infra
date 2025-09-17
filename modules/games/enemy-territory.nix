6bc8badd3b18ce22870c2de45e3aec2a980cc783
Pol Dellaiera <pol.dellaiera@protonmail.com>
2025-09-17 17:41:52 +0200

    games: add desktop shortcut

--- modules/games/enemy-territory.nix
+++ modules/games/enemy-territory.nix
@@ -1,11 +1,19 @@
 {
   flake.modules = {
     homeManager.games =
-      { pkgs, ... }:
+      { pkgs, lib, ... }:
       {
         home.packages = with pkgs; [
           etlegacy
         ];
+
+        xdg.desktopEntries.et-fr-beginner-xps = {
+          name = "Enemy territory @ France Beginner XPS";
+          exec = "${lib.getExe' pkgs.etlegacy "etl"} +connect 46.105.209.160:27960";
+          icon = "etl";
+          categories = [ "Game" ];
+          terminal = false;
+        };
       };
   };
 
