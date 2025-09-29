38830b0906d0528d87210a7a4f127dc45bd83d5c
Pol Dellaiera <pol.dellaiera@protonmail.com>
2025-09-29 21:12:48 +0200

    open-webui: remove obsolete stuff

--- modules/ai/open-webui.nix
+++ modules/ai/open-webui.nix
@@ -5,61 +5,55 @@
     ];
   };
 
-  flake.modules.nixos.ai =
-    { config, ... }:
-    {
-      services = {
-        open-webui = {
-          enable = true;
-          host = "0.0.0.0";
-          port = 8080;
-          environment = {
-            # Until https://github.com/NixOS/nixpkgs/issues/430433#issuecomment-3146683267 has been resolved
-            FRONTEND_BUILD_DIR = "${config.services.open-webui.stateDir}/build";
-            DATA_DIR = "${config.services.open-webui.stateDir}/data";
-            STATIC_DIR = "${config.services.open-webui.stateDir}/static";
-            CONTENT_EXTRACTION_ENGINE = "tika";
-            DEVICE_TYPE = "cpu";
-            ENABLE_OLLAMA_API = "True";
-            ENABLE_OPENAI_API = "True";
-            ENABLE_RAG_HYBRID_SEARCH = "True";
-            ENABLE_RAG_WEB_LOADER_SSL_VERIFICATION = "False";
-            ENABLE_RAG_WEB_SEARCH = "True";
-            OLLAMA_BASE_URL = "http://127.0.0.1:11434";
-            OLLAMA_API_BASE_URL = "http://127.0.0.1:11434/api";
-            OPENAI_API_BASE_URL = "http://127.0.0.1:8888/v1";
-            OPENAI_API_KEY = "";
-            PDF_EXTRACT_IMAGES = "True";
-            RAG_EMBEDDING_ENGINE = "ollama";
-            RAG_EMBEDDING_MODEL = "embeddinggemma:latest";
-            RAG_EMBEDDING_MODEL_AUTO_UPDATE = "True";
-            RAG_FILE_MAX_COUNT = "2";
-            RAG_RERANKING_MODEL = "BAAI/bge-reranker-v2-m3";
-            RAG_TEXT_SPLITTER = "token";
-            RAG_WEB_SEARCH_ENGINE = "searxng";
-            RAG_WEB_SEARCH_RESULT_COUNT = "5";
-            RESET_CONFIG_ON_START = "True";
-            SEARXNG_QUERY_URL = "http://apollo:3002/search?q=<query>";
-            TIKA_SERVER_URL = "http://apollo:9998/";
-            WEBUI_AUTH = "False";
-            WEBUI_NAME = "LLM @ Home";
-          };
-        };
-
-        caddy = {
-          enable = true;
-          virtualHosts."http://".extraConfig = ''
-            reverse_proxy 127.0.0.1:8080
-          '';
-          virtualHosts."https://".extraConfig = ''
-            reverse_proxy 127.0.0.1:8080
-          '';
+  flake.modules.nixos.ai = {
+    services = {
+      open-webui = {
+        enable = true;
+        host = "0.0.0.0";
+        port = 8080;
+        environment = {
+          CONTENT_EXTRACTION_ENGINE = "tika";
+          DEVICE_TYPE = "cpu";
+          ENABLE_OLLAMA_API = "True";
+          ENABLE_OPENAI_API = "True";
+          ENABLE_RAG_HYBRID_SEARCH = "True";
+          ENABLE_RAG_WEB_LOADER_SSL_VERIFICATION = "False";
+          ENABLE_RAG_WEB_SEARCH = "True";
+          OLLAMA_BASE_URL = "http://127.0.0.1:11434";
+          OLLAMA_API_BASE_URL = "http://127.0.0.1:11434/api";
+          OPENAI_API_BASE_URL = "http://127.0.0.1:8888/v1";
+          OPENAI_API_KEY = "";
+          PDF_EXTRACT_IMAGES = "True";
+          RAG_EMBEDDING_ENGINE = "ollama";
+          RAG_EMBEDDING_MODEL = "embeddinggemma:latest";
+          RAG_EMBEDDING_MODEL_AUTO_UPDATE = "True";
+          RAG_FILE_MAX_COUNT = "2";
+          RAG_RERANKING_MODEL = "BAAI/bge-reranker-v2-m3";
+          RAG_TEXT_SPLITTER = "token";
+          RAG_WEB_SEARCH_ENGINE = "searxng";
+          RAG_WEB_SEARCH_RESULT_COUNT = "5";
+          RESET_CONFIG_ON_START = "True";
+          SEARXNG_QUERY_URL = "http://apollo:3002/search?q=<query>";
+          TIKA_SERVER_URL = "http://apollo:9998/";
+          WEBUI_AUTH = "False";
+          WEBUI_NAME = "LLM @ Home";
         };
       };
 
-      networking.firewall.allowedTCPPorts = [
-        80
-        443
-      ];
+      caddy = {
+        enable = true;
+        virtualHosts."http://".extraConfig = ''
+          reverse_proxy 127.0.0.1:8080
+        '';
+        virtualHosts."https://".extraConfig = ''
+          reverse_proxy 127.0.0.1:8080
+        '';
+      };
     };
+
+    networking.firewall.allowedTCPPorts = [
+      80
+      443
+    ];
+  };
 }
