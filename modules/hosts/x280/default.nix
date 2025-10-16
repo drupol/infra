15506f3aa9a0cf98406aabc1eab5b1a032eab607
Pol Dellaiera <pol.dellaiera@protonmail.com>
2025-10-16 20:17:54 +0200

    x280: update config

--- modules/hosts/x280/default.nix
+++ modules/hosts/x280/default.nix
@@ -3,34 +3,171 @@
   ...
 }:
 {
-  flake.modules.nixos."hosts/x280".imports =
-    with config.flake.modules.nixos;
-    [
-      # Modules
-      base
-      bluetooth
-      desktop
-      dev
-      facter
-      fwupd
-      shell
-      sound
-      vpn
+  flake.modules.homeManager.user =
+    { lib, ... }:
+    {
+      programs.plasma = {
+        fonts = lib.mkForce { };
 
-      # Users
-      root
-      pol
-    ]
-    # Specific Home-Manager modules
-    ++ [
-      {
-        home-manager.users.pol.imports = with config.flake.modules.homeManager; [
-          base
-          desktop
-          dev
-          facter
-          shell
+        input.keyboard.layouts = lib.mkForce [
+          {
+            layout = "be";
+          }
         ];
-      }
-    ];
+
+        configFile = {
+          plasma-localerc = lib.mkForce {
+            Formats = {
+              LANG = "fr_BE.UTF-8";
+            };
+          };
+        };
+      };
+
+      xdg.desktopEntries = {
+        whatsapp = {
+          type = "Application";
+          name = "Whatsapp";
+          genericName = "Messenger";
+          comment = "Whatsapp";
+          exec = "firefox --new-window https://web.whatsapp.com";
+          icon = ./WhatsApp.svg;
+          terminal = false;
+          categories = [
+            "AudioVideo"
+            "Network"
+          ];
+        };
+        messages = {
+          type = "Application";
+          name = "Messages";
+          genericName = "Messenger";
+          comment = "Google Messages Web";
+          exec = "firefox --new-window https://messages.google.com/web/conversations";
+          icon = ./Google_Messages.svg;
+          terminal = false;
+          categories = [
+            "AudioVideo"
+            "Network"
+          ];
+        };
+        crelan = {
+          type = "Application";
+          name = "Crelan";
+          genericName = "Banking";
+          comment = "Crelan Online Banking";
+          exec = "firefox --new-window https://mycrelan.crelan.be/";
+          icon = ./crelan.svg;
+          terminal = false;
+          categories = [
+            "Network"
+            "Office"
+          ];
+        };
+      };
+
+      programs.firefox.languagePacks = lib.mkForce [ "fr" ];
+      programs.firefox.profiles.default.settings."intl.locale.requested" = lib.mkForce "fr,it";
+      programs.firefox.profiles.default.settings."intl.accept_languages" = lib.mkForce "fr,it";
+      programs.firefox.profiles.default.settings."font.name.monospace.x-western" = lib.mkForce "";
+      programs.firefox.profiles.default.settings."font.name.sans-serif.x-western" = lib.mkForce "";
+      programs.firefox.profiles.default.settings."font.name.serif.x-western" = lib.mkForce "";
+
+      programs.thunderbird = {
+        settings = {
+          "intl.locale.requested" = lib.mkForce "fr,it";
+          "intl.accept_languages" = lib.mkForce "fr,it";
+        };
+      };
+    };
+
+  flake.modules.nixos."hosts/x280" =
+    { pkgs, lib, ... }:
+    {
+      imports =
+        with config.flake.modules.nixos;
+        [
+          # Modules
+          base
+          bluetooth
+          desktop
+          facter
+          fwupd
+          openssh
+          sound
+          vpn
+
+          # Users
+          root
+          user
+        ]
+        # Specific Home-Manager modules
+        ++ [
+          {
+            home-manager.users.user.imports = with config.flake.modules.homeManager; [
+              base
+              desktop
+              facter
+              user
+            ];
+          }
+        ];
+
+      boot.loader = {
+        systemd-boot.enable = true;
+        efi.canTouchEfiVariables = true;
+        efi.efiSysMountPoint = "/boot/efi";
+      };
+
+      services = {
+        xserver = {
+          xkb = {
+            layout = "be";
+          };
+        };
+        thermald.enable = true;
+        avahi.enable = true;
+      };
+
+      facter.reportPath = ./facter.json;
+
+      fileSystems = {
+        "/" = {
+          device = "/dev/disk/by-uuid/89a4586a-eefb-4dd4-bf06-3953902edc1e";
+          fsType = "ext4";
+        };
+
+        "/boot/efi" = {
+          device = "/dev/disk/by-uuid/155B-2355";
+          fsType = "vfat";
+        };
+
+        "/home" = {
+          device = "/dev/disk/by-uuid/ce407b75-260e-47f0-822e-1984866571db";
+          fsType = "ext4";
+        };
+
+        "/nix" = {
+          device = "/dev/disk/by-uuid/c56d5d01-df37-471e-8827-dc193ceb182b";
+          fsType = "ext4";
+        };
+      };
+
+      swapDevices = [ { device = "/dev/disk/by-uuid/005040e5-7773-438e-8ede-f3f63a242d7d"; } ];
+
+      environment.systemPackages = with pkgs; [
+        thunderbird
+        libreoffice
+      ];
+
+      system.autoUpgrade = lib.mkForce {
+        enable = true;
+        flake = "https://github.com/drupol/infra";
+        allowReboot = true;
+      };
+
+      i18n.defaultLocale = lib.mkForce "fr_BE.UTF-8";
+
+      fonts.packages = lib.mkForce [ ];
+    };
 }
