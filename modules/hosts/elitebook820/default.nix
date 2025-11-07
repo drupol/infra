be27e8bc3e41d81f336c53aa528b5b379bed98b3
Pol Dellaiera <pol.dellaiera@protonmail.com>
2025-11-07 07:34:10 +0100

    elitebook820: revive

---
+++ modules/hosts/elitebook820/default.nix
@@ -0,0 +1,85 @@
+{
+  config,
+  lib,
+  ...
+}:
+{
+  flake.modules.nixos."hosts/elitebook820" = {
+    imports =
+      with config.flake.modules.nixos;
+      [
+        # Modules
+        base
+        bluetooth
+        desktop
+        dev
+        facter
+        fwupd
+        shell
+        sound
+        vpn
+
+        # Users
+        root
+        pol
+        demo
+      ]
+      # Specific Home-Manager modules
+      ++ [
+        {
+          home-manager.users.demo = {
+            imports = with config.flake.modules.homeManager; [
+              base
+              desktop
+              dev
+              facter
+              shell
+              demo
+            ];
+          };
+
+          home-manager.users.pol = {
+            imports = with config.flake.modules.homeManager; [
+              base
+              desktop
+              dev
+              facter
+              shell
+              pol
+            ];
+          };
+        }
+      ];
+
+    boot = {
+      loader = {
+        systemd-boot.enable = true;
+        efi.canTouchEfiVariables = true;
+      };
+
+      initrd.availableKernelModules = [
+        "xhci_pci"
+        "ahci"
+        "usb_storage"
+        "sd_mod"
+        "rtsx_pci_sdmmc"
+      ];
+
+      kernelModules = [ "kvm-intel" ];
+    };
+
+    fileSystems."/" = {
+      device = "/dev/disk/by-uuid/0831c17a-27d6-42b8-a61b-f52cfb02f051";
+      fsType = "ext4";
+    };
+
+    fileSystems."/boot" = {
+      device = "/dev/disk/by-uuid/5185-45FD";
+      fsType = "vfat";
+    };
+
+    swapDevices = [ { device = "/dev/disk/by-uuid/066ce479-3611-42e7-9117-f1ef77668010"; } ];
+
+    hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  };
+}
