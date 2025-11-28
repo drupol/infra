7abd63d57e7ade8c7c6027995fac5d0c84cf0734
Pol Dellaiera <pol.dellaiera@protonmail.com>
2025-11-28 12:04:53 +0100

    imac: update config

--- modules/hosts/imac/default.nix
+++ modules/hosts/imac/default.nix
@@ -63,7 +63,10 @@
         { device = "/dev/disk/by-uuid/c9e3a23f-d2c6-49c7-94ad-0372aa4f94e5"; }
       ];
 
-      services.xserver.videoDrivers = [ "nvidia" ];
+      services.xserver.videoDrivers = [
+        "nvidia"
+        "intel"
+      ];
 
       # Enable sound with pipewire.
       services.pulseaudio.enable = false;
@@ -79,40 +82,57 @@
 
       nixpkgs.config.nvidia.acceptLicense = true;
       nixpkgs.config.allowBroken = true;
-      hardware.graphics.enable = true;
-      hardware.nvidia = {
+      hardware = {
+        opengl = {
+          enable = true;
+          driSupport = true;
+          driSupport32Bit = true;
+        };
+        graphics.enable = true;
+        nvidia = {
+          # Optionally, you may need to select the appropriate driver version for your specific GPU.
+          package = pkgs.linuxPackages_latest.nvidiaPackages.legacy_390;
 
-        # Modesetting is required.
-        modesetting.enable = true;
+          # Modesetting is required.
+          modesetting.enable = true;
 
-        # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
-        # Enable this if you have graphical corruption issues or application crashes after waking
-        # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead
-        # of just the bare essentials.
-        powerManagement.enable = false;
+          # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+          # Enable this if you have graphical corruption issues or application crashes after waking
+          # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead
+          # of just the bare essentials.
+          powerManagement.enable = true;
 
-        # Fine-grained power management. Turns off GPU when not in use.
-        # Experimental and only works on modern Nvidia GPUs (Turing or newer).
-        powerManagement.finegrained = false;
+          # Fine-grained power management. Turns off GPU when not in use.
+          # Experimental and only works on modern Nvidia GPUs (Turing or newer).
+          powerManagement.finegrained = false;
 
-        # Use the NVidia open source kernel module (not to be confused with the
-        # independent third-party "nouveau" open source driver).
-        # Support is limited to the Turing and later architectures. Full list of
-        # supported GPUs is at:
-        # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
-        # Only available from driver 515.43.04+
-        # Currently "beta quality", so false is currently the recommended setting.
-        open = false;
+          # Use the NVidia open source kernel module (not to be confused with the
+          # independent third-party "nouveau" open source driver).
+          # Support is limited to the Turing and later architectures. Full list of
+          # supported GPUs is at:
+          # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
+          # Only available from driver 515.43.04+
+          # Currently "beta quality", so false is currently the recommended setting.
+          open = false;
 
-        # Enable the Nvidia settings menu,
-        # accessible via `nvidia-settings`.
-        nvidiaSettings = true;
-
-        # Optionally, you may need to select the appropriate driver version for your specific GPU.
-        package = pkgs.linuxPackages_latest.nvidiaPackages.legacy_390;
+          # Enable the Nvidia settings menu,
+          # accessible via `nvidia-settings`.
+          nvidiaSettings = true;
+        };
       };
 
-      boot.extraModulePackages = [ pkgs.linuxPackages_latest.broadcom_sta ];
+      boot.blacklistedKernelModules = [
+        "nouveau"
+        "rivafb"
+        "nvidiafb"
+        "rivatv"
+        "nv"
+        "uvcvideo"
+      ];
+      boot.extraModulePackages = [
+        pkgs.linuxPackages_latest.broadcom_sta
+        config.boot.kernelPackages.nvidia_x11
+      ];
 
       nixpkgs.config.permittedInsecurePackages = [
         "broadcom-sta-6.30.223.271-59-6.17.9"
