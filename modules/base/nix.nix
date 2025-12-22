326f8c4754d3474f069c614d182dc2f4327bb4af
Pol Dellaiera <pol.dellaiera@protonmail.com>
2025-12-22 11:10:33 +0100

    nix: fix config discrepancy

--- modules/base/nix.nix
+++ modules/base/nix.nix
@@ -6,7 +6,7 @@
         # See https://discourse.nixos.org/t/24-05-add-flake-to-nix-path/46310/9
         # See https://hachyderm.io/@leftpaddotpy/112539055867932912
         channel.enable = false;
-        settings.nix-path = [ "nixpkgs=${pkgs.path}" ];
+        nixPath = [ "nixpkgs=${pkgs.path}" ];
 
         # From https://jackson.dev/post/nix-reasonable-defaults/
         extraOptions = ''
