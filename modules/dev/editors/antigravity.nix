3b9edf592aa6637960c4170f6e63a17ae2360dc0
Pol Dellaiera <pol.dellaiera@protonmail.com>
2025-12-22 10:09:01 +0100

    dev: add antigravity

---
+++ modules/dev/editors/antigravity.nix
@@ -0,0 +1,29 @@
+{
+  lib,
+  ...
+}:
+{
+  flake.modules = {
+    nixos.dev = {
+      services = {
+        gnome.gnome-keyring.enable = true;
+      };
+    };
+
+    homeManager.dev =
+      { pkgs, ... }:
+      {
+        nixpkgs = {
+          config = {
+            allowUnfree = true;
+          };
+        };
+
+        home.packages = with pkgs; [
+          # Using the FHS version to allow installation of extensions
+          # I don't really need Nix to manage extensions for me
+          antigravity-fhs
+        ];
+      };
+  };
+}
