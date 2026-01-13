803721c79676a85d6135002d4c2d45f0ecf6c90b
Pol Dellaiera <pol.dellaiera@protonmail.com>
2026-01-13 08:52:20 +0100

    noise-station-server: update

--- modules/noise-station-server/default.nix
+++ modules/noise-station-server/default.nix
@@ -15,6 +15,7 @@
           openFirewall = true;
           settings = {
             server.http_addr = "0.0.0.0";
+            server.http_port = 3000;
           };
           provision = {
             enable = true;
