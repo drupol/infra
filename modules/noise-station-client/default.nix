4163b9e8461e51240e9fca53a5506f8108dc0c73
Pol Dellaiera <pol.dellaiera@protonmail.com>
2026-01-21 23:25:00 +0100

    noise-station-client: update Telegraf config

--- modules/noise-station-client/default.nix
+++ modules/noise-station-client/default.nix
@@ -16,41 +16,49 @@
       users.groups.dialout.members = [ "telegraf" ];
       users.groups.plugdev.members = [ "telegraf" ];
 
-      services = {
-        telegraf = {
-          enable = true;
-          extraConfig = {
-            inputs.exec = {
-              commands = [
-                "${lib.getExe pkgs.local.temper-py} --json"
-              ];
-              data_format = "json";
-              interval = "60s";
-            };
-            inputs.execd = {
-              command = [
-                "${lib.getExe pkgs.local.dt8852}"
-                "live"
-                "--range"
-                "R_30_80"
-                "--freqweighting"
-                "dbc"
-                "--timeweighting"
-                "slow"
-                "--format"
-                "telegraf"
-                "-v"
-              ];
-              signal = "none";
-            };
-            outputs.influxdb_v2 = {
-              urls = [ "http://192.168.2.116:8086" ];
-              token = "noisestation";
-              organization = "default";
-              bucket = "default";
+      services =
+        let
+          temper-py-bin = lib.getExe (
+            pkgs.writeScriptBin "read-temper.sh" ''
+              ${lib.getExe pkgs.local.temper-py} --json
+            ''
+          );
+        in
+        {
+          telegraf = {
+            enable = true;
+            extraConfig = {
+              inputs.exec = {
+                commands = [
+                  "${temper-py-bin}"
+                ];
+                data_format = "json";
+                interval = "60s";
+              };
+              inputs.execd = {
+                command = [
+                  "${lib.getExe pkgs.local.dt8852}"
+                  "live"
+                  "--range"
+                  "R_30_80"
+                  "--freqweighting"
+                  "dbc"
+                  "--timeweighting"
+                  "slow"
+                  "--format"
+                  "telegraf"
+                  "-v"
+                ];
+                signal = "none";
+              };
+              outputs.influxdb_v2 = {
+                urls = [ "http://192.168.2.116:8086" ];
+                token = "noisestation";
+                organization = "default";
+                bucket = "default";
+              };
             };
           };
         };
-      };
     };
 }
