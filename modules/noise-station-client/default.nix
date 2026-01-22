2b2d4ac92ee37a621192dd0b7651bb36a735916a
Pol Dellaiera <pol.dellaiera@protonmail.com>
2026-01-22 19:54:59 +0100

    noise-station-client: switch to `temper`

--- modules/noise-station-client/default.nix
+++ modules/noise-station-client/default.nix
@@ -18,97 +18,82 @@
       users.groups.dialout.members = [ "telegraf" ];
       users.groups.plugdev.members = [ "telegraf" ];
 
-      services =
-        let
-          temper-py-bin = lib.getExe (
-            pkgs.writeShellApplication {
-              name = "read-temper-bin";
-              runtimeInputs = [
-                pkgs.local.temper-py
-                pkgs.jq
+      services = {
+        udev.extraRules = ''
+          KERNEL=="hidraw*", SUBSYSTEM=="hidraw", MODE="0660", GROUP="telegraf"
+        '';
+        telegraf = {
+          enable = true;
+          extraConfig = {
+            inputs.exec = {
+              commands = [
+                "${lib.getExe pkgs.local.temper} --json --force 3553:a001 | ${lib.getExe pkgs.jq} '.[0]'"
               ];
-              text = ''
-                ${lib.getExe pkgs.local.temper-py} --json | jq '.[0] | with_entries(.key |= gsub(" "; "_"))'
-              '';
-            }
-          );
-        in
-        {
-          udev.extraRules = ''
-            KERNEL=="hidraw*", SUBSYSTEM=="hidraw", MODE="0660", GROUP="telegraf"
-          '';
-          telegraf = {
-            enable = true;
-            extraConfig = {
-              inputs.exec = {
-                commands = [
-                  "${temper-py-bin}"
-                ];
-                interval = "60s";
-                data_format = "json_v2";
+              interval = "60s";
+              data_format = "json_v2";
 
-                json_v2 = [
-                  {
-                    measurement_name = "usb_temperature";
-
-                    tag = [
-                      { path = "product"; }
-                      { path = "port"; }
-                      { path = "vendorid"; }
-                      { path = "firmware"; }
-                    ];
-
-                    field = [
-                      {
-                        path = "internal_temperature";
-                        rename = "temp_internal";
-                        type = "float";
-                      }
-                      {
-                        path = "external_temperature";
-                        rename = "temp_external";
-                        type = "float";
-                      }
-                    ];
-                  }
-                ];
-              };
-
-              inputs.execd = {
-                command = [
-                  "${lib.getExe pkgs.local.dt8852}"
-                  "live"
-                  "--range"
-                  "R_30_80"
-                  "--freqweighting"
-                  "dba"
-                  "--timeweighting"
-                  "slow"
-                  "--format"
-                  "telegraf"
-                  "-v"
-                ];
-                signal = "none";
-              };
-
-              outputs.influxdb_v2 = [
+              json_v2 = [
                 {
-                  urls = [ "http://192.168.2.116:8086" ];
-                  token = "noisestation";
-                  organization = "default";
-                  bucket = "default";
-                  namedrop = [ "usb_temperature" ];
-                }
-                {
-                  urls = [ "http://192.168.2.116:8086" ];
-                  token = "noisestation";
-                  organization = "default";
-                  bucket = "usb_temperature";
-                  namepass = [ "usb_temperature" ];
+                  measurement_name = "usb_temperature";
+
+                  tag = [
+                    { path = "product"; }
+                    { path = "port"; }
+                    { path = "vendorid"; }
+                    { path = "firmware"; }
+                  ];
+
+                  field = [
+                    {
+                      path = "internal_temperature";
+                      rename = "temp_internal";
+                      type = "float";
+                    }
+                    {
+                      path = "external_temperature";
+                      rename = "temp_external";
+                      type = "float";
+                    }
+                  ];
                 }
               ];
             };
+
+            inputs.execd = {
+              command = [
+                "${lib.getExe pkgs.local.dt8852}"
+                "live"
+                "--range"
+                "R_30_80"
+                "--freqweighting"
+                "dba"
+                "--timeweighting"
+                "slow"
+                "--format"
+                "telegraf"
+                "-v"
+              ];
+              signal = "none";
+            };
+
+            outputs.influxdb_v2 = [
+              {
+                urls = [ "http://192.168.2.116:8086" ];
+                token = "noisestation";
+                organization = "default";
+                bucket = "default";
+                namedrop = [ "usb_temperature" ];
+              }
+              {
+                urls = [ "http://192.168.2.116:8086" ];
+                token = "noisestation";
+                organization = "default";
+                bucket = "usb_temperature";
+                namepass = [ "usb_temperature" ];
+              }
+            ];
           };
         };
+      };
     };
 }
