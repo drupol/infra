3cc554b5e6254381c1179e270537259c0cec70da
Pol Dellaiera <pol.dellaiera@protonmail.com>
2026-03-12 19:07:05 +0100

    Revert "networking: wpa_supplicant -> iwd"
    This reverts commit f300e766e26886cf59ee3bc16bcc35d54b4c0fb2.

--- modules/base/network/default.nix
+++ modules/base/network/default.nix
@@ -7,14 +7,6 @@
 
         networkmanager = {
           enable = true;
-          wifi.backend = "iwd";
-        };
-        wireless.iwd = {
-          enable = true;
-          settings = {
-            IPv6.Enabled = true;
-            Settings.AutoConnect = true;
-          };
         };
       };
 
