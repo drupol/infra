39ba0c252eaaaca41a90184b3b5f4a2f04fba58c
den.aspects.pol <pol.dellaiera@protonmail.com>
2026-04-13 08:19:19 +0200

    json-sort-cli: enable correctly now

--- modules/flake-parts/fmt.nix
+++ modules/flake-parts/fmt.nix
@@ -6,12 +6,13 @@
   ];
 
   perSystem =
-    { self', pkgs, ... }:
+    { self', ... }:
     {
       treefmt = {
         projectRootFile = "flake.nix";
         programs = {
           deadnix.enable = true;
+          json-sort-cli.enable = true;
           jsonfmt.enable = true;
           nixfmt.enable = true;
           prettier.enable = true;
@@ -20,21 +21,6 @@
           yamlfmt.enable = true;
         };
         settings = {
-          formatter = {
-            "json-sort-cli" = {
-              command = "${lib.getExe pkgs.bash}";
-              options = [
-                "-euc"
-                ''
-                  for file in "$@"; do
-                    ${lib.getExe pkgs.json-sort-cli} $file --insert-final-newline true --autofix || true
-                  done
-                ''
-                "--" # bash swallows the second argument when using -c
-              ];
-              includes = [ "*.json" ];
-            };
-          };
           on-unmatched = "warn";
         };
       };
