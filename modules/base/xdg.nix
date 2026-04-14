1c9842e235978cfdb2c38a32aff1ca515fd42e5c
den.aspects.pol <pol.dellaiera@protonmail.com>
2026-04-14 08:48:55 +0200

    xdg: update module

--- modules/base/xdg.nix
+++ modules/base/xdg.nix
@@ -1,21 +1,29 @@
 {
   flake.modules = {
-    homeManager.base = {
-      xdg = {
-        enable = true;
-        mime.enable = true;
-        userDirs = {
+    homeManager.base =
+      { pkgs, ... }:
+      {
+        xdg = {
           enable = true;
-          createDirectories = true;
-          templates = null;
-          music = null;
-          videos = null;
-          publicShare = null;
+          mime.enable = true;
+          userDirs = {
+            enable = true;
+            createDirectories = true;
+            setSessionVariables = true;
+            templates = null;
+            music = null;
+            videos = null;
+            publicShare = null;
+          };
+          autostart.enable = true;
+          mimeApps.enable = true;
         };
-        autostart = {
-          enable = true;
+
+        home = {
+          preferXdgDirectories = true;
+          # Checks $HOME for unwanted files and directories.
+          packages = with pkgs; [ xdg-ninja ];
         };
       };
-    };
   };
 }
