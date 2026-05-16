2478101e3281fad22db476dd7f388a8aab1f4e5d
Pol Dellaiera <pol.dellaiera@protonmail.com>
2026-05-17 00:55:36 +0200

    nix: update config

--- modules/base/nix.nix
+++ modules/base/nix.nix
@@ -1,37 +1,33 @@
 {
   den.aspects.base = {
-    nixos =
-      { pkgs, ... }:
-      {
-        nix = {
-          # See https://discourse.nixos.org/t/24-05-add-flake-to-nix-path/46310/9
-          # See https://hachyderm.io/@leftpaddotpy/112539055867932912
-          channel.enable = false;
-          nixPath = [ "nixpkgs=${pkgs.path}" ];
+    nixos = {
+      nix = {
+        # See https://discourse.nixos.org/t/24-05-add-flake-to-nix-path/46310/9
+        # See https://hachyderm.io/@leftpaddotpy/112539055867932912
+        channel.enable = false;
+        nixPath = [ "nixpkgs=flake:nixpkgs" ];
 
+        optimise.automatic = true;
+        settings = {
+          use-xdg-base-directories = true;
+          trusted-users = [
+            "root"
+          ];
+          auto-optimise-store = true;
+          experimental-features = [
+            "nix-command"
+            "flakes"
+          ];
+          warn-dirty = false;
+          tarball-ttl = 60 * 60 * 24;
           # From https://jackson.dev/post/nix-reasonable-defaults/
-          extraOptions = ''
-            connect-timeout = 5
-            log-lines = 50
-            min-free = 128000000
-            max-free = 1000000000
-            fallback = true
-          '';
-          optimise.automatic = true;
-          settings = {
-            use-xdg-base-directories = true;
-            trusted-users = [
-              "root"
-            ];
-            auto-optimise-store = true;
-            experimental-features = [
-              "nix-command"
-              "flakes"
-            ];
-            warn-dirty = false;
-            tarball-ttl = 60 * 60 * 24;
-          };
+          connect-timeout = 5;
+          log-lines = 50;
+          min-free = 128000000;
+          max-free = 1000000000;
+          fallback = true;
         };
       };
+    };
   };
 }
