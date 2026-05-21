2084593dba843050d6b0894ceeae51caabfe9a9c
Pol Dellaiera <pol.dellaiera@protonmail.com>
2026-05-21 09:50:58 +0200

    ai-local: update and fixes

--- modules/ai-local/mcp.nix
+++ modules/ai-local/mcp.nix
@@ -6,41 +6,43 @@
   den.aspects.ai-local = {
     homeManager =
       { pkgs, ... }:
+      let
+        mcp-server-time = pkgs.mcp-server-time.overrideAttrs {
+          meta.mainProgram = "mcp-server-time";
+        };
+        mcp-server-sequential-thinking = pkgs.mcp-server-sequential-thinking.overrideAttrs {
+          meta.mainProgram = "mcp-server-sequential-thinking";
+        };
+      in
       {
         programs.mcp = {
           enable = true;
           servers = {
             mcp-server-sequential-thinking = {
-              type = "local";
-              command = [ (lib.getExe pkgs.mcp-mcp-server-sequential-thinking) ];
+              command = lib.getExe mcp-server-sequential-thinking;
               enabled = false;
             };
             mcp-server-memory = {
-              type = "local";
-              command = [ (lib.getExe pkgs.mcp-server-memory) ];
+              command = lib.getExe pkgs.mcp-server-memory;
               enabled = false;
             };
             mcp-server-git = {
-              type = "local";
-              command = [ (lib.getExe pkgs.mcp-server-git) ];
+              command = lib.getExe pkgs.mcp-server-git;
               enabled = false;
             };
             mcp-server-filesystem = {
-              type = "local";
-              command = [ (lib.getExe pkgs.mcp-server-filesystem) ];
+              command = lib.getExe pkgs.mcp-server-filesystem;
               enabled = false;
             };
             mcp-server-fetch = {
-              type = "local";
-              command = [
-                (lib.getExe pkgs.mcp-server-fetch)
+              command = lib.getExe pkgs.mcp-server-fetch;
+              args = [
                 "--ignore-robots-txt"
               ];
               enabled = false;
             };
             mcp-server-time = {
-              type = "local";
-              command = [ (lib.getExe pkgs.mcp-server-time) ];
+              command = lib.getExe mcp-server-time;
               enabled = false;
             };
           };
---
+++ modules/ai-local/opencode.nix
@@ -0,0 +1,17 @@
+{
+  den.aspects.ai-local = {
+    homeManager = {
+      programs.opencode = {
+        enable = true;
+        enableMcpIntegration = true;
+        web = {
+          enable = false;
+        };
+        settings = {
+          autoshare = false;
+          autoupdate = false;
+        };
+      };
+    };
+  };
+}
