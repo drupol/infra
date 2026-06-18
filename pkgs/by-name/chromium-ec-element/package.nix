6db2a938a09559e7ea733b78fc737f581c317dac
Pol Dellaiera <pol.dellaiera@protonmail.com>
2026-06-18 11:29:34 +0200

    message: use new matrix URL

--- pkgs/by-name/chromium-ec-element/package.nix
+++ pkgs/by-name/chromium-ec-element/package.nix
@@ -7,8 +7,8 @@ pkgs.nix-webapps-lib.mkChromiumApp {
     "Chat"
     "InstantMessaging"
   ];
-  class = "chrome-chat.contact.ec.europa.eu__-Default";
+  class = "chrome-app.matrix.tech.ec.europa.eu__-Default";
   desktopName = "Matrix @ European Commission";
   icon = ./Element_logo.svg;
-  url = "https://chat.contact.ec.europa.eu";
+  url = "https://app.matrix.tech.ec.europa.eu";
 }
