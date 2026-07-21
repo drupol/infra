25f763209ed7b2044cd4eef9a6c65c9b1b9599e3
Pol Dellaiera <pol.dellaiera@protonmail.com>
2026-07-21 22:20:13 +0200

    wifi: more stability for iwd

--- modules/wifi/default.nix
+++ modules/wifi/default.nix
@@ -17,7 +17,19 @@
         inputs.infra-private.nixosModules.wifi
       ];
 
-      networking.networkmanager.wifi.backend = "iwd";
+      networking = {
+        networkmanager.wifi.backend = "iwd";
+
+        wireless.iwd = {
+          enable = true;
+
+          settings.Rank = {
+            BandModifier2_4GHz = 2.0; # (v > 1) = 2.4 GHz networks are preferred over the rest
+            BandModifier5GHz = 0.5; # (0 < v < 1): preferred over 6 GHz networks
+            BandModifier6GHz = 0.1; # (0 < v < 1): the least preferred band, but still usable if nothing else is available
+          };
+        };
+      };
     };
   };
 }
