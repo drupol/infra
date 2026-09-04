45f25898ea4da720bf7ecf21aab698dc4322f611
Pol Dellaiera <pol.dellaiera@protonmail.com>
2026-09-04 21:52:48 +0200

    base: update `journald` config

--- modules/base/services.nix
+++ modules/base/services.nix
@@ -8,10 +8,10 @@
 
         # Limit the systemd journal to 100 MB of disk or the
         # last 3 days of logs, whichever happens first.
-        journald.extraConfig = ''
-          SystemMaxUse=100M
-          MaxFileSec=3day
-        '';
+        journald.settings.Journal = {
+          SystemMaxUse = "100M";
+          MaxFileSec = "3day";
+        };
 
         nscd.enableNsncd = true;
       };
